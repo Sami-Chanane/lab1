258523ebdd437b16ca411e8bd718dde3969a4a51
chsami <samichanane@Samis-MacBook-Air.local>
2024-04-18 08:12:56 +0200

    added comment to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+//this is just a comment to test ignoring index on master
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
