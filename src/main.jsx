7291f761c721f97c5e95b397f43628bca24eb899
chsami <samichanane@Samis-MacBook-Air.local>
2024-04-18 08:19:15 +0200

    added comment in main.jsx in dev-1

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+//this comment was added in dev-1 to test the "push on main.jsx in dev-1" event
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
